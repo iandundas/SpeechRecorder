3be1c04f53ea16bd2687b6f2f449d2038e7dafd3
Ian Dundas <1131967+iandundas@users.noreply.github.com>
2023-11-15 09:12:10 +0100

    Initial Commit

---
+++ SpeechRecorder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpeechRecorder
+//
+//  Created by Ian Dundas on 15/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpeechRecorder/SpeechRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechRecorderApp.swift
+//  SpeechRecorder
+//
+//  Created by Ian Dundas on 15/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpeechRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
